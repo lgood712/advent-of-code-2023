72d7e937e3d33310d4be01fc4da847653848f0d2
Luke Good <lukegood1993@gmail.com>
2023-12-15 16:59:14 -0500

    day12p1

---
+++ day12/solution.py
@@ -0,0 +1,43 @@
+#https://adventofcode.com/2023/day/12
+
+with open("input") as f:
+    data = f.read().strip().splitlines()
+
+# Part 1 
+def dfs(sequence, groups):
+    if not groups:
+        return '#' not in sequence
+    seq_len = len(sequence)
+    group_len = groups[0]
+    if seq_len - sum(groups) - len(groups) + 1 < 0:
+        return 0
+    has_holes = any(sequence[x] == '.' for x in range(group_len))
+    if seq_len == group_len:
+        return 0 if has_holes else 1
+    can_use = not has_holes and (sequence[group_len] != '#')
+    if sequence[0] == '#':
+        return dfs(sequence[group_len+1:].lstrip('.'), tuple(groups[1:])) if can_use else 0
+    skip = dfs(sequence[1:].lstrip('.'), groups)
+    if not can_use:
+        return skip
+    return skip + dfs(sequence[group_len+1:].lstrip('.'), tuple(groups[1:]))
+
+    
+
+
+def p1results(data):
+    total = 0
+    for line in data:
+        sequence, groups = line.split()
+        groups = tuple(int(g) for g in groups.split(','))
+        total += dfs(sequence, groups)
+    return total
+
+print("P1 RESULT: ", p1results(data))
+
+
+# Part 2
+def p2results(data):
+    return
+
+print("P2 RESULT: ", p2results(data))
